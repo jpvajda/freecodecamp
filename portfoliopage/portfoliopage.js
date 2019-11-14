1e51f84c4e3307e94b3f28daa2a7e4ef5aba915a
John P. Vajda <john.vajda@gmail.com>
2019-11-14 14:27:26 -0700

    added jquery library and contact us email integration with AWS

--- portfoliopage/portfoliopage.js
+++ portfoliopage/portfoliopage.js
@@ -23,3 +23,53 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+// contact us form email integration with AWS
+
+function submitToAPI(e) {
+    e.preventDefault();
+    var URL = "https://auhtj9l5xd.execute-api.us-east-2.amazonaws.com/beta";
+
+         var Namere = /[A-Za-z]{1}[A-Za-z]/;
+         if (!Namere.test($("#name").val())) {
+                      alert ("Name can not less than 2 characters");
+             return;
+         }
+    
+         if ($("#email").val()=="") {
+             alert ("Please enter your email id");
+             return;
+         }
+
+         var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+         if (!reeamil.test($("#email").val())) {
+             alert ("Please enter valid email address");
+             return;
+         }
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var data = {
+       name : name,
+       email : email,
+     };
+
+    $.ajax({
+      type: "POST",
+      url : "https://auhtj9l5xd.execute-api.us-east-2.amazonaws.com/beta",
+      dataType: "json",
+      crossDomain: "true",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+
+      success: function () {
+        // clear form and show a success message
+        alert("Successful");
+        document.getElementById("contact-form").reset();
+    location.reload();
+      },
+      error: function () {
+        // show an error message
+        alert("UnSuccessful");
+      }});
+  }
