8945018829700d6f977bcc4f61cddb04ada678f8
John P. Vajda <john.vajda@gmail.com>
2019-12-03 11:57:35 -0700

    CSS, and javascript improvments

--- quotemachine/quotemachine.js
+++ quotemachine/quotemachine.js
@@ -1,1 +1,24 @@
-// starter file for javascript
+// quote generator code
+
+$(document).ready(function(){
+  getNewQuote();
+});
+
+ $(".quote-btn").click(function(){
+   getNewQuote();
+ });
+
+ function getNewQuote(){
+   $.ajax({
+  type: 'GET',
+  dataType: 'json',
+   cache: false,
+  url: 'http://quotes.stormconsultancy.co.uk/random.json',
+  success: function(data){
+    $(".quote").show();
+    $(".author-name").show();
+    $(".text").html(data.quote + "<br>");
+    $(".author-name").html(data.author);
+  }
+});   
+  }
