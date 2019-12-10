f23bdec204cf7ec07ffaa0477dbb1fe021a22dcd
John P. Vajda <john.vajda@gmail.com>
2019-12-10 10:36:48 -0700

    added javascript sound library + css improvements

--- drummachine/drummachine.js
+++ drummachine/drummachine.js
@@ -1,48 +1,88 @@
-const keyBank = [{
-  keyCode: 0,
-  keyTrigger: 'Q',
-  id: 'Heater-1',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-}, {
-  keyCode: 1,
-  keyTrigger: 'W',
-  id: 'Heater-2',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-}, {
-  keyCode: 2,
-  keyTrigger: 'E',
-  id: 'Heater-3',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-}, {
-  keyCode: 3,
-  keyTrigger: 'A',
-  id: 'Heater-4',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-}, {
-  keyCode: 4,
-  keyTrigger: 'S',
-  id: 'Clap',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-}, {
-  keyCode: 5,
-  keyTrigger: 'D',
-  id: 'Open-HH',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-}, {
-  keyCode: 6,
-  keyTrigger: 'Z',
-  id: "Kick-n'-Hat",
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-}, {
-  keyCode: 7,
-  keyTrigger: 'X',
-  id: 'Kick',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-}, {
-  keyCode: 8,
-  keyTrigger: 'C',
-  id: 'Closed-HH',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-},
-];
+
+// Sound library 
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3";
+    obj.id = 'Heater-1';
+    $("#Q").click(function() {
+      obj.play();
+    });
+  });
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3";
+    obj.id = 'Heater-2';
+    $("#W").click(function() {
+      obj.play();
+    });
+  });
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3";
+    obj.id = 'Heater-3';
+    $("#E").click(function() {
+      obj.play();
+    });
+  });
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3";
+    obj.id = 'Heater-4';
+    $("#A").click(function() {
+      obj.play();
+    });
+  });
+
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3";
+    obj.id = 'Clap';
+    $("#S").click(function() {
+      obj.play();
+    });
+  });
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3";
+    obj.id = 'Open-HH';
+    $("#D").click(function() {
+      obj.play();
+    });
+  });
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3";
+    obj.id = 'Kick-n-Hat';
+    $("#Z").click(function() {
+      obj.play();
+    });
+  });
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3";
+    obj.id = 'Kick';
+    $("#X").click(function() {
+      obj.play();
+    });
+  });
+
+  $(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src = "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3";
+    obj.id = 'Closed-HH';
+    $("#C").click(function() {
+      obj.play();
+    });
+  });
+
+
+
 
