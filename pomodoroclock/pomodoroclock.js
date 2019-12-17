918b2f6fc25c6c98ee65b6a4533edb326b432233
John P. Vajda <john.vajda@gmail.com>
2019-12-17 15:10:28 -0700

    jS changes

--- pomodoroclock/pomodoroclock.js
+++ pomodoroclock/pomodoroclock.js
@@ -1,15 +1,59 @@
-// JS Starter file 
+$(document).ready(function () {
+  var beep = $("#beep");
+  var breakCount = parseInt($("#break-length").html());
+  var sessionCount = parseInt($("#session-length").html());
+  var timeCount = parseInt($("#time-left").html());
 
-$(document).ready(function(){ 
-  var buzzer =$("#beep")[0];
-  var count = parseInt($("#break-length").html());
- 
-$("#reset").hide(); 
+  // timer functionality 
 
-$("#break-decrement").click(function() { 
- count -= 1;
- $("#break-length").html(count);
-});
+  $("#start_stop").click(function (){ 
+    var counter = setInterval(timer, 1000);
+
+    function timer() { 
+      timeCount -= 1;
+      if(timeCount === 0){ 
+        beep.play();
+        clearInterval(counter)
+      }
+
+      $("#time-left").html(count);
+    }
+  });
+
+  // break time controls
+
+  $("#break-increment").click(function () {
+    if (breakCount < 60) {
+      breakCount += 1;
+      $("#break-length").html(breakCount);
+    }
+  });
+
+  $("#break-decrement").click(function () {
+    if (breakCount > 1) {
+      breakCount -= 1;
+      $("#break-length").html(breakCount);
+    }
+  });
+
+
+  // session time controls
+
+  $("#session-increment").click(function () {
+    if (sessionCount < 60) {
+      sessionCount += 1;
+      $("#session-length").html(sessionCount);
+    }
+  });
+
+  $("#session-decrement").click(function () {
+    if (sessionCount > 1) {
+      sessionCount -= 1;
+      $("#session-length").html(sessionCount);
+    }
+  });
+
+
 
 });
 
