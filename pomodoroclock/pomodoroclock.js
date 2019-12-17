4bda77281b30073fafed1bc1d2140c3e440a2dfc
John P. Vajda <john.vajda@gmail.com>
2019-12-17 12:18:05 -0700

    JS and CSS improvements

--- pomodoroclock/pomodoroclock.js
+++ pomodoroclock/pomodoroclock.js
@@ -1,1 +1,16 @@
 // JS Starter file 
+
+$(document).ready(function(){ 
+  var buzzer =$("#beep")[0];
+  var count = parseInt($("#break-length").html());
+ 
+$("#reset").hide(); 
+
+$("#break-decrement").click(function() { 
+ count -= 1;
+ $("#break-length").html(count);
+});
+
+});
+
+
