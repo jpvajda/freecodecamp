f7dcdf3e92b26e3819a9118517f42ec284a2447a
John P. Vajda <john.vajda@gmail.com>
2020-01-02 17:04:58 -0700

    finished header parser endpoint

--- API _microservices_projects/headerParser_microservice.js
+++ API _microservices_projects/headerParser_microservice.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -13,21 +14,25 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// enables bodyparser 
+app.use(bodyParser.json());
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
 // GET endpoint to return header info
-app.get("/api/whoami/:request?", function(req, res) { 
-  res.json({greeting: 'is working!'});
+app.get("/api/whoami/", function(req, res, next) { 
+  var language = req.acceptsLanguages();
+  var software = req.get('User-Agent');
+  var ipaddress = req.ip;
+  res.json({'ip': ipaddress, 'language': language, 'software': software});
 });
 
 
