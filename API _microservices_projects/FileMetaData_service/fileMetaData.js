27a78079a5d3950ba3f1c882e73999c7df76187c
John P. Vajda <john.vajda@gmail.com>
2020-01-07 12:52:17 -0700

    completed code for metadata service, + portfolio page update

--- API _microservices_projects/FileMetaData_service/fileMetaData.js
+++ API _microservices_projects/FileMetaData_service/fileMetaData.js
@@ -1,1 +1,29 @@
-// code goes here 
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+var upload = multer({dest:'uploads/'});
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/upload', upload.single('file'),(req, res, next) => { 
+  return res.json(req.file);
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
