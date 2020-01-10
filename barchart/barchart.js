36a9392d7331c3e0b31c1a69eea1ea299bf7bb6f
John P. Vajda <john.vajda@gmail.com>
2020-01-09 17:02:35 -0700

    more experimentation with d3

--- barchart/barchart.js
+++ barchart/barchart.js
@@ -45,32 +45,64 @@ var barChart = svg1.selectAll('rect')
 
     // example chart 2
 
-    var dataset = [1,2,3,4,5];
+  var dataset = [1,2,3,4,5];
 
-    var svgWidth = 500, svgHeight = 300, barPadding = 5;
-    var barWidth = (svgWidth / dataset.length);
+  var svgWidth = 500, svgHeight = 300, barPadding = 5;
+  var barWidth = (svgWidth / dataset.length);
 
-    var svg2 = d3.select('#svg2')
-      .attr('width', svgWidth)
-      .attr('height', svgHeight);
+  var svg2 = d3.select('#svg2')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
 
-    var yScale = d3.scaleLinear()
-      .domain([0, d3.max(dataset)])
-      .range([0, svgHeight])
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgHeight])
 
-    var barChart = svg2.selectAll('rect')
-      .data(dataset)
-      .enter()
-      .append('rect')
-      .attr('y', function(d) { 
-        return svgHeight - d - yScale(d)
-      })
-
-    .attr('height', function(d){ 
-        return yScale(d);
+  var barChart = svg2.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('y', function(d) { 
+      return svgHeight - d - yScale(d)
     })
-      .attr('width', barWidth - barPadding)
-      .attr('transform', function (d, i) { 
-          var translate = [barWidth * i, 0];
-          return 'translate('+ translate + ')';
-      });
+
+  .attr('height', function(d){ 
+      return yScale(d);
+  })
+    .attr('width', barWidth - barPadding)
+    .attr('transform', function (d, i) { 
+        var translate = [barWidth * i, 0];
+        return 'translate('+ translate + ')';
+    });
+
+// example 3 
+
+var data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var svgWidth = 500, svgHeight = 300;
+
+var svg3 = d3.select('#svg3')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
+var xScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, svgWidth]);
+ 
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([svgHeight, 0]);
+
+var x_axis = d3.axisBottom().scale(xScale);
+
+var y_axis = d3.axisLeft().scale(yScale);
+
+svg3.append('g')
+    .attr('transform', 'translate(50,10)')
+    .call(y_axis);
+
+var xAxisTranslate = svgHeight - 20;
+
+svg3.append('g')
+    .attr('transform', 'translate(50,' + xAxisTranslate +')')
+    .call(x_axis);
