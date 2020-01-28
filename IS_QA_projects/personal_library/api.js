fe07500a4722a966b2eadada9f9426dc7774306f
John P. Vajda <john.vajda@gmail.com>
2020-01-28 16:25:02 -0700

    updated  project files for personal library project

--- IS_QA_projects/personal_library/api.js
+++ IS_QA_projects/personal_library/api.js
@@ -4,36 +4,33 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 const MONGODB_CONNECTION_STRING = process.env.DB;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 module.exports = function (app) {
 
-  // Book object constructor 
+ 
+// Book Schema 
 
-// function Book() { 
-//   this.title = String,
-//   this.comments = [String],
-//   this.commentcount =  { type: Number, default: 0 }
-// };
-
-//   let Book = new Book;
-
-const Book = new Book({
+const bookSchema = new mongoose.Schema({
     title: String,
     comments: [String],
     commentcount: { type: Number, default: 0 }
 });
   
+var Book = mongoose.model('Book', bookSchema);
   
   // get all books 
-  app.route('/api/books')
+  app.route('/api/books') 
     .get(function (req, res){
      Book.find({}, (err, docs) => { 
        if (err) throw err;
        res.send(docs);
+       console.log(docs);
       })
     })
   
-    // create a new bookitle
+    // create a new book
     .post(function (req, res){
       const title = req.body.title;
       if (!title) return res.send('no title given');
@@ -43,41 +40,44 @@ const Book = new Book({
       })
     })
   
-    // delete all books    
-    .delete(function(req,res) { 
+     // delete all books    
+     .delete(function(req,res) { 
       Book.deleteMany({}, (err, docs) => { 
         if (err) return res.send('complete delete unsuccessful');
         res.send('complete delete successful')
       })
     })
   
-    // get a single book);
+    // get a single book
   app.route('/api/books/:id')
-    .get(function (req, res){
-    const {_id} = req.params;
-    Book.findById(_id, (err, doc) => { 
+    .get(function (req, res, next){
+    const id = req.body.id; 
+    Book.findById(id, (err, doc) => { 
         if (err || !doc) return res.send('no book exists');
         res.send(doc);
+        console.log(id);
+        console.log(doc);
       })
     })
 
     // create a comment 
     .post(function(req, res) {
-      const {_id} = req.params; 
-      const { comment } = req.body;
-      Book.findByIdAndUpdate(_id), { $push: { comments: comment }, $inc: { commentcount: 1}}, { new: true}, (err, doc) => {
+      const id = req.body.id; 
+      const comment = req.body.comment;
+      Book.findByIdAndUpdate(id, { $push: { comments: comment }, $inc: { commentcount: 1}}, { new: true}, (err, doc) => {
         if (err) return res.send('no book exists');
-          res.send(doc);
-      } 
+           res.send(doc);  
+          console.log(id);
+          console.log(doc);  
+      }) 
     })
-  
+
     // delete a single book 
     .delete(function(req, res){  
-      const  {_id} = req.params;
-      Book.findByAndRemove(_id, err => { 
+      const id = req.body.id;
+      Book.findByIdAndRemove(id, err => { 
         if (err) return res.send('no book exists');
         res.send('complete delete successful');
       })
-    });
-  
+    }); 
 };
