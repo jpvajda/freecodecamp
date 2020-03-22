72db754a8ab9f25994de34c704fd913552ef6aa0
John P. Vajda <john.vajda@gmail.com>
2020-03-21 18:14:49 -0600

    added bar chart elements and final code

--- barchart/barchart.js
+++ barchart/barchart.js
@@ -1,193 +1,97 @@
-
-// // example chart 1 
-// var dataset = [80,100,56,120,180,36,40,120,160];
-
-// var svgWidth = 500, svgHeight = 300, barPadding = 5;
-// var barWidth = (svgWidth / dataset.length);
-
-// var svg1 = d3.select('#svg1')
-//   .attr('width', svgWidth)
-//   .attr('height', svgHeight)
-
-// var barChart = svg1.selectAll('rect')
-//   .data(dataset)
-//   .enter()
-//   .append('rect')
-//   .attr('class', 'bar')
-//   .attr("fill", 'blue')
-//   .attr('y', function(d) { 
-//     return svgHeight - d;
-//   })
-//   .attr('height', function(d){ 
-//       return d;
-//   })
-//   .attr('width', barWidth - barPadding)
-//   .attr('transform', function (d, i) { 
-//     var translate = [barWidth * i, 0];
-//       return 'translate(' + translate +')';
+req=new XMLHttpRequest();
+req.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json',true);
+req.send();
+req.onload=function(){
+  json=JSON.parse(req.responseText);
+  var dataset = json.data;
+  
+  const w = 1000;
+  const h = 500;
+  const padding = h*0.1;
+  
+  const svg = d3.select("body")
+                .append("svg")
+                .attr("id", "title")
+                .attr("width", w)
+                .attr("height", h)
+                .style("padding", "50px");
+  
+  var datesMap = dataset.map(function(each) {
+    return new Date(each[0]);
+  });
+  
+  var dateMax = new Date(d3.max(datesMap));
+  dateMax.setMonth(dateMax.getMonth() + 3);
+  
+  var GDP = dataset.map(function(each) {
+    return each[1];
+  });
+  
+  var parsedGDP = [];
+  
+  var linearScale = d3.scaleLinear()
+                      .domain([0, d3.max(GDP)])
+                      .range([0, h - padding - 50]);
+  
+  parsedGDP = GDP.map(function(each) {
+    return linearScale(each);
+  });
+  
+  const xScale = d3.scaleTime()
+                   .domain([d3.min(datesMap), dateMax])
+                   .range([padding, w - padding]);
+  
+  const xAxis = d3.axisBottom().scale(xScale);
+  
+  svg.append("g")
+    .attr("id", "x-axis")
+    .attr("transform", "translate(0, " + (h - padding) + ")")
+    .call(xAxis);
+  
+  const yScale = d3.scaleLinear()
+                   .domain([d3.max(GDP), 0])
+                   .range([padding, h - padding]);
+  
+  const yAxis = d3.axisLeft(yScale);
+  
+  svg.append("g") 
+     .attr("id", "y-axis")
+     .attr("transform", "translate(" + (padding) + ", 0)")
+     .call(yAxis);
+  
+  var tooltip = d3.select("body")
+	                .append("div")
+	                .style("position", "absolute")
+	                .style("z-index", "10")
+	                .style("visibility", "visible");
+                  
+  svg.selectAll("rect")
+     .data(parsedGDP)
+     .enter()
+     .append("rect")
    
-//   });
-
-//   var text = svg1.selectAll('text')
-//     .data(dataset)
-//     .enter()
-//     .append('text')
-//     .attr("fill", 'red')
-//     .text(function(d) { 
-//       return d;
-//     })
-//     .attr('y', function(d, i){ 
-//       return svgHeight - d - 2;
-//     })
-//     .attr('x', function(d, i) { 
-//       return barWidth * i;
-//     });
-
-//     // example chart 2
-
-//   var dataset = [1,2,3,4,5];
-
-//   var svgWidth = 500, svgHeight = 300, barPadding = 5;
-//   var barWidth = (svgWidth / dataset.length);
-
-//   var svg2 = d3.select('#svg2')
-//     .attr('width', svgWidth)
-//     .attr('height', svgHeight);
-
-//   var yScale = d3.scaleLinear()
-//     .domain([0, d3.max(dataset)])
-//     .range([0, svgHeight])
-
-//   var barChart = svg2.selectAll('rect')
-//     .data(dataset)
-//     .enter()
-//     .append('rect')
-//     .attr('y', function(d) { 
-//       return svgHeight - d - yScale(d)
-//     })
-
-//   .attr('height', function(d){ 
-//       return yScale(d);
-//   })
-//     .attr('width', barWidth - barPadding)
-//     .attr('transform', function (d, i) { 
-//         var translate = [barWidth * i, 0];
-//         return 'translate('+ translate + ')';
-//     });
-
-// example 3 
-
-// var data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-
-// var svgWidth = 500, svgHeight = 300;
-
-// var svg3 = d3.select('#svg3')
-//     .attr('width', svgWidth)
-//     .attr('height', svgHeight);
-
-// var xScale = d3.scaleLinear()
-//     .domain([0, d3.max(data)])
-//     .range([0, svgWidth]);
- 
-// var yScale = d3.scaleLinear()
-//     .domain([0, d3.max(data)])
-//     .range([svgHeight, 0]);
-
-// var x_axis = d3.axisBottom().scale(xScale);
-
-// var y_axis = d3.axisLeft().scale(yScale);
-
-// svg3.append('g')
-//     .attr('transform', 'translate(50,10)')
-//     .call(y_axis);
-
-//  // example 4    
-
-// var xAxisTranslate = svgHeight - 20;
-
-// svg3.append('g')
-//     .attr('transform', 'translate(50,' + xAxisTranslate +')')
-//     .call(x_axis);
-
-//     var width = 400,
-//         height = 100;
-
-//     var data = [10, 15, 20, 25, 30];
-    
-//     // Append SVG 
-//     var svg = d3.select('#x-axis')
-//                 .append("svg")
-//                 .attr("width", width)
-//                 .attr("height", height);
-
-//     // Create scale
-//     var scale = d3.scaleLinear()
-//                   .domain([d3.min(data), d3.max(data)])
-//                   .range([0, width - 100]);
-
-//     // Add scales to axis
-//     var x_axis = d3.axisBottom()
-//                    .scale(scale);
-
-//     //Append group and insert axis
-//     svg.append("g")
-//        .call(x_axis)
-//        .attr("id", "x-axis");
+     .attr('x', (d, i) => xScale(datesMap[i]))
+     .attr('y', (d) => h - padding - d)
    
-//    // Sets Y-Axis
+     .attr("width", 4)
+     .attr("height", (d) => d)
+     .attr("class", "bar")
    
-//    var width = 400, height = 400;
-
-//    var data = [10, 15, 20, 25, 30];
-//    var svg = d3.select("#y-axis")
-//                .append("svg")
-//                .attr("width", width)
-//                .attr("height", height);
-
-//    var scale = d3.scaleLinear()
-//                  .domain([d3.min(data), d3.max(data)])
-//                  .range([height/2, 0]);
-
-//    var y_axis = d3.axisLeft()
-//                  .scale(scale);
-
-//    svg.append("g")
-//       .attr("transform", "translate(50, 10)")
-//       .call(y_axis)
-//       .attr("id", "y-axis");
-
-  // example 5 
-
-	var width = 400, height = 400;
-	var data = [10, 15, 20, 25, 30];
-	
-	var svg = d3.select("body")
-		.append("svg")
-		.attr("width", width)
-		.attr("height", height);
-
-	var xscale = d3.scaleLinear()
-		.domain([0, d3.max(data)])
-		.range([0, width - 100]);
-
-	var yscale = d3.scaleLinear()
-			.domain([0, d3.max(data)])
-			.range([height/2, 0]);
-
-	var x_axis = d3.axisBottom()
-			.scale(xscale);
-
-	var y_axis = d3.axisLeft()
-			.scale(yscale);
-
-		svg.append("g")
-		   .attr("transform", "translate(50, 10)")
-       .call(y_axis)
-       .attr("id", "y-axis");
-
-	var xAxisTranslate = height/2 + 10;
-
-		svg.append("g")
-				.attr("transform", "translate(50, " + xAxisTranslate  +")")
-        .call(x_axis)
-        .attr("id", "x-axis");
+     .attr('data-date', (d, i) => dataset[i][0])
+     .attr('data-gdp', (d, i) => dataset[i][1])
+   
+  	 .on("mouseover", function(d, i){
+     return tooltip.attr("class", "tooltip")
+                   .style("visibility", "visible")
+                   .attr("id", "tooltip")
+                   .attr('data-date', dataset[i][0])
+                   .html(dataset[i][0] + "\n" + dataset[i][1] + "$");
+})
+     .on("mousemove", function(){
+     return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
+})
+     .on("mouseout", function(){
+     return tooltip.attr("class", "")
+                  .style("visibility", "hidden");
+})
+};
