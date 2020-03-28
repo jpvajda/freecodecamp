3efb806d4d89aa9a82f336c37d5d840d22945ab0
John P. Vajda <john.vajda@gmail.com>
2020-03-28 12:08:52 -0600

    new approach to scatterplot project

--- scatterplot/scatterplot.js
+++ scatterplot/scatterplot.js
@@ -1,78 +1,60 @@
+import {
+  select,
+  csv,
+  scaleLinear,
+  extent,
+  axisLeft,
+  axisBottom,
+  format
+} from 'd3';
+
+// requests data from source 
 req=new XMLHttpRequest();
 req.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json',true);
 req.send();
 req.onload=function(){
   json=JSON.parse(req.responseText);
   var dataset = json.data;
-  
-// Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 270 - margin.top - margin.bottom;
 
-// Parse the date / time
-var parseDate = d3.time.format("%d-%b-%y").parse;
 
-// Set the ranges
-var x = d3.time.scale().range([0, width]);
-var y = d3.scale.linear().range([height, 0]);
+  // sets canvas dimensions 
+  const svg = select('svg');
+  const width = +svg.attr('width');
+  const height = +svg.attr('height');
 
-// Define the axes
-var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(5);
+  const render = data => { 
 
-var yAxis = d3.svg.axis().scale(y)
-    .orient("left").ticks(5);
+    //sets title
+    const title = 'FCC: Scatterplot Graph';
 
-// Define the line
-var valueline = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
-    
-// Adds the svg canvas
-var svg = d3.select("body")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
+    //set x-axis
+    const xValue = d => d.year;
+    const xAxisLabel = 'Year';
 
-// Get the data
-d3.json(dataset, function(error, data) {
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.close = +d.close;
+    //set y-axis
+    const yValue = d => d.time;
+    const circleRadius = 4;
+    const yAxisLabel = 'Time in Minutes';
+
+    const margin = { top: 60, right: 40, bottom: 88, left: 150 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+
+  };
+
+  // gets data from csv
+  csv('data.csv')
+  .then(data => {
+    data.forEach(d => {
+      d.mpg = +d.mpg;
+      d.cylinders = +d.cylinders;
+      d.displacement = +d.displacement;
+      d.horsepower = +d.horsepower;
+      d.weight = +d.weight;
+      d.acceleration = +d.acceleration;
+      d.year = +d.year;  
     });
-
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.close; })]);
-
-    // Add the valueline path.
-    svg.append("path")
-        .attr("class", "line")
-        .attr("d", valueline(data));
-
-    // Add the scatterplot
-    svg.selectAll("dot")
-        .data(data)
-      .enter().append("circle")
-        .attr("r", 3.5)
-        .attr("cx", function(d) { return x(d.date); })
-        .attr("cy", function(d) { return y(d.close); });
-
-    // Add the X Axis
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("id", "x-axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    // Add the Y Axis
-    svg.append("g")
-        .attr("class", "y axis")
-        .attr("id", "y-axis")
-        .call(yAxis);
-      });
-    // }
+    render(data);
+  });
+}
